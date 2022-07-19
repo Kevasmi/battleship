0579fbf50c1434a77c7c5c584bea42186a886b1b
Richard Lasco <lasco9@gmail.com>
2022-07-19 15:39:53 -0500

    initial setup of project files and plugins

---
+++ bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('123');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
