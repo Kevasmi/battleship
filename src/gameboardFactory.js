f56c0ecff791c45e0579c969af50d6dc8a753823
Richard Lasco <lasco9@gmail.com>
2022-07-20 18:39:05 -0500

    Init creation of gameboardFactory and first test

---
+++ src/gameboardFactory.js
@@ -0,0 +1,26 @@
+const createGameboard = () => {
+  return {
+    board: [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ],
+    missedAttacks: [],
+    numOfShips: 5,
+    allShipsSunk: false,
+    placeShip(coordinates) {},
+    receiveAttack(coordiante) {},
+    areAllShipsSunk() {
+      this.allShipsSunk = true;
+    },
+  };
+};
+
+export { createGameboard };
---
+++ src/gameboardFactory.test.js
@@ -0,0 +1,27 @@
+import { createGameboard } from './gameboardFactory';
+
+describe('createGameboard function', () => {
+  test('should return a gameBoard object', () => {
+    const expectedBoard = {
+      board: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ],
+      missedAttacks: [],
+      numOfShips: 5,
+      allShipsSunk: false,
+    };
+
+    let testBoard = createGameboard();
+
+    expect(testBoard).toMatchObject(expectedBoard);
+  });
+});
