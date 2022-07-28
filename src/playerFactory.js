dfc379c7c67f56f284ac286c9f5dac3d071a9723
Richard Lasco <lasco9@gmail.com>
2022-07-28 10:29:42 -0500

    Fixed randomCoord&attack funcs + added DummyBoard

--- src/playerFactory.js
+++ src/playerFactory.js
@@ -2,29 +2,30 @@ import { createGameboard } from './gameboardFactory';
 
 function randomCoordinate() {
   const array = [];
-  const firstNum = Math.floor(Math.random * 9);
-  const secondNum = Math.floor(Math.random * 10);
+  const firstNum = Math.floor(Math.random() * 9);
+  const secondNum = Math.floor(Math.random() * 10) + 1;
   array.push(firstNum, secondNum);
   return array;
 }
 
 const createPlayer = ({ name = 'Anonymous', isComputer = false } = {}) => ({
   name: name,
-  gameBoard: createGameboard(),
   isAComputer: isComputer,
+  gameBoard: createGameboard(),
+  dummyBoard: createGameboard().board,
   playerAttack(coordinate, enemy) {
     enemy.gameBoard.receiveAttack(coordinate);
+    enemy.computerAttack(this);
   },
   computerAttack(enemy) {
     try {
       let coordinate = randomCoordinate();
-      enemy.board.receiveAttack(coordinate);
+      enemy.gameBoard.receiveAttack(coordinate);
     } catch (e) {
       console.error(e);
       this.computerAttack();
     }
   },
-  nextRound() {},
 });
 
 export { createPlayer };
