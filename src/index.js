af35548794ee85c22908bacf0ac57f318165bdf5
Richard Lasco <lasco9@gmail.com>
2022-08-02 19:15:26 -0500

    Set-up main game flow

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('123');
+import { bindSubmitEvent } from './bindEvents';
+import { computerPlayerArray } from './makeComputerPlayer';
+import { renderComputerBoard, renderPlayerBoard } from './renderBoards';
+
+let isHorizontal = true;
+const randomNumber = Math.floor(Math.random() * 2);
+let computer = computerPlayerArray[randomNumber];
+bindSubmitEvent(computer, isHorizontal);
+renderPlayerBoard();
+renderComputerBoard();
